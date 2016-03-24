36eff66b1b76bd28ca78fc0f48fc9ee11840bbac
nikigmy <nikigmi@gmail.com>
2016-03-24 12:32:31 +0200

    Added a Movement script and an InputHandler.

---
+++ StarWars/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public static bool Arming;
+    public static bool Sprinting;
+    public static bool Jumping;
+    public static bool Attacking;
+    public static float VerticalInput;
+    public static float HorizontalInput;
+    public static float MouseXAxis;
+    public static float MouseYAxis;
+
+	void Update ()
+	{
+        VerticalInput = Input.GetAxis("Vertical");
+        HorizontalInput = Input.GetAxis("Horizontal");
+        Sprinting = Input.GetButton("Sprint") && VerticalInput != 0;
+        Jumping = Input.GetButtonDown("Jump");
+	    Arming = Input.GetButtonDown("Arming");
+	    Attacking = Input.GetMouseButtonDown(0);
+	    MouseXAxis = Input.GetAxis("Mouse X");
+	    MouseYAxis = Input.GetAxis("Mouse Y");
+	    if (!Sprinting) return;
+	    VerticalInput *= 2;
+	    HorizontalInput = 0;
+	}
+}
--- StarWars/Assets/Scripts/MoveAnimation.cs
+++ StarWars/Assets/Scripts/MoveAnimation.cs
@@ -3,38 +3,22 @@
 public class MoveAnimation : MonoBehaviour
 {
     private Animator _animator;
-    private float _verticalInput;
-    private float _horizontalInput;
     private float _rotateSpeed = 10f;
-    private bool _runing;
     private bool _moving;
-    private bool _jump;
 
     private void Start()
     {
-        //Inicialization
         this._animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        //Getting input 
-        this._verticalInput = Input.GetAxis("Vertical");
-        this._horizontalInput = Input.GetAxis("Horizontal");
-        this._runing = Input.GetButton("Sprint");
-        this.transform.Rotate(0f, Input.GetAxis("Mouse X") * this._rotateSpeed, 0f);
-        this._jump = Input.GetButtonDown("Jump");
+        this.transform.Rotate(0f, InputHandler.MouseXAxis * this._rotateSpeed, 0f);
     }
 
     private void FixedUpdate()
     {
-        if (this._runing && (this._verticalInput > 0 || this._verticalInput < 0))
-        {
-            //You cant steer while sprinting
-            this._verticalInput *= 2;
-            this._horizontalInput = 0;
-        }
-        if (this._verticalInput > 0 || this._verticalInput < 0 || this._horizontalInput > 0 || this._horizontalInput < 0)
+         if(InputHandler.VerticalInput > 0 || InputHandler.VerticalInput < 0 || InputHandler.HorizontalInput > 0 || InputHandler.HorizontalInput < 0)
             this._moving = true;
         else this._moving = false;
         Animate();
@@ -42,13 +26,13 @@ public class MoveAnimation : MonoBehaviour
 
     private void Animate()
     {
-        if (this._jump)
+        if (InputHandler.Jumping)
         {
             this._animator.SetTrigger("Jump");
-            this._jump = false;
+            InputHandler.Jumping = false;
         }
         this._animator.SetBool("Moving", this._moving);
-        this._animator.SetFloat("InputY", this._verticalInput);
-        this._animator.SetFloat("InputX", this._horizontalInput);
+        this._animator.SetFloat("InputY", InputHandler.VerticalInput);
+        this._animator.SetFloat("InputX", InputHandler.HorizontalInput);
     }
 }
