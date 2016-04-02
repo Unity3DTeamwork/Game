13ea26f300638a5e402a33105344785c21c0f3e4
nikigmy <nikigmi@gmail.com>
2016-04-02 23:51:06 +0300

    Fixed the repository.

---
+++ StarWars/Assets/Resources/Scripts/CharacterMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        private CharacterController _controller;
+        private Vector3 _desiredMove;
+        [SerializeField]
+        private float _mouseSensitivity = 10f;
+        [SerializeField]
+        private float _gravity = 10f;
+        private Animator _animator;
+
+
+        private void Start()
+        {
+            this._controller = GetComponent<CharacterController>();
+            this._animator = GetComponent<Animator>();
+        }
+
+
+        private void Update()
+        {
+            AnimateMovement();
+        }
+        private void FixedUpdate()
+        {
+            Character.Grounged = this._controller.isGrounded;
+            this._desiredMove = Vector3.zero;
+            if (this._controller.isGrounded)
+            {
+                if (Character.Jump )
+                {
+                    Jump();
+                }
+                else
+                {
+                    Run();
+                }
+            }
+
+            if (!Character.Jumping)
+                this._desiredMove.y = -this._gravity;
+
+            this._controller.Move(this._desiredMove);
+            this.transform.Rotate(0f, InputHandler.MouseXAxis * this._mouseSensitivity, 0f);
+        }
+
+        private void Jump()
+        {
+            Character.Jumping = true;
+            Character.Jump = false;
+            this._animator.SetTrigger("Jump");
+        }
+
+        private void Run()
+        {
+            this._desiredMove = this.transform.forward * Character.VerticalSpeed /
+                                (Character.Armed ? 10 : 5) +
+                                this.transform.right * Character.HorizontalSpeed /
+                                (Character.Armed ? 10 : 5);
+        }
+
+        private void AnimateMovement()
+        {
+            this._animator.SetBool("Moving", Character.Moving);
+            this._animator.SetFloat("InputY", Character.VerticalSpeed);
+            this._animator.SetFloat("InputX", Character.HorizontalSpeed);
+        }
+        public void FinishedJumping()
+        {
+            Character.Jumping = false;
+        }
+    }
+}
---
+++ StarWars/Assets/Resources/Scripts/WeaponAnimating.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class WeaponAnimating : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform _pants;
+        [SerializeField]
+        private Transform _hand;
+        [SerializeField]
+        private Transform _lightSaber;
+
+        private Animator _anim;
+        private GameObject _lightsaberBlade;
+
+        private void Start()
+        {
+            this._anim = GetComponent<Animator>();
+            this._anim.SetBool("Armed", Character.Armed);
+            this._lightsaberBlade = this._lightSaber.GetChild(0).gameObject;
+        }
+
+        private void Update()
+        {
+            if (InputHandler.Arm && !Character.Attacking && Character.Grounged && !Character.Arming && !Character.Jumping)
+            {
+                Arm();
+            }
+            if (!InputHandler.Attack || Character.Attacking || !Character.Armed || Character.Arming) return;
+            Attack();
+        }
+
+        private void Arm()
+        {
+            Character.Armed = !Character.Armed;
+            this._anim.SetTrigger(Character.Armed ? "Arm" : "Disarm");
+            this._anim.SetBool("Armed", Character.Armed);
+            Character.Arming = true;
+        }
+
+        private void Attack()
+        {
+            Character.Attacking = true;
+            this._anim.SetTrigger("Attack");
+        }
+
+        public void FinishedAttacking()
+        {
+            Character.Attacking = false;
+        }
+
+        public void FinishedArmingDisarming()
+        {
+            Character.Arming = false;
+        }
+
+        public void GetLeaveLightsaber()
+        {
+            this._lightSaber.SetParent(this._lightSaber.parent == this._hand ? this._pants : this._hand);
+            this._lightSaber.localPosition = Vector3.zero;
+            this._lightSaber.localRotation = Quaternion.identity;
+        }
+        public void TurnLightsaberOnOff()
+        {
+            this._lightsaberBlade.SetActive(!this._lightsaberBlade.activeSelf);
+        }
+    }
+}
